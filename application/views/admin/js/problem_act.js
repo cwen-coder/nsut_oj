40b472c046e6f188470d782c741654d19fc0dce7
yin-coder <172569856@qq.com>
2015-05-18 23:19:20 +0800

    完成安题号，标题查找

--- application/views/admin/js/problem_act.js
+++ application/views/admin/js/problem_act.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
 
     //获取点击删除模态框的所在行的id
@@ -20,6 +21,88 @@ $(document).ready(function() {
       })
     });
 
-    
+    $("#search_but").click(function() {
+       var search = $("#search").val();
+       if(search.length == 0) {
+        alert("请输入搜索关键字");
+       } else {
+          var checked = $('input:radio[name="search"]:checked').val();
+          if(checked == 1) {
+            search_id(search);
+          } else if(checked == 2) {
+            search_title(search);
+          } else {
+            search_class(search);
+          }
+       }
+    });
     
 });
+
+function search_id(search) {
+    var regx = /^[0-9]*$/;
+    if(!regx.test(search)) {
+      alert("请输入正确题号");
+      return 
+    } else {
+      var url = $("#hid_site").val() + '/admin/problem/search_problem_byId';
+      $.post(url, {
+        problem_id : search
+      },function(data) {
+        if(data == false) {
+          alert("请输入正确题号");
+        }else if(data == 1) {
+          $("#tbody").html("");
+          $("#links").remove();
+          alert("抱歉没有找到");
+        } else {
+          var pro = eval("("+data+")");
+          $("#tbody").html("");
+          $("#links").remove();
+          var tr = $("<tr></tr>");
+          tr.appendTo(tbody);
+          td = $("<td>"+ pro.problem_id +"</td>");
+          td.appendTo(tr);
+          td = $("<td>"+ pro.title +"</td>");
+          td.appendTo(tr);
+          td = $("<td>"+ pro.class_name +"</td>");
+          td.appendTo(tr);
+          var url_ed = $("#hid_site").val() + '/admin/problem/pro_edit/' + pro.problem_id;
+          td = $("<td>"+ "<a href='" + url_ed + "'><i class='icon-pencil'></i></a>" +
+           "  <a href='#myModal' role='button' data-toggle='modal' id = '" + pro.problem_id + "'><i class='icon-remove'></i></a>" 
+           + "</td>");
+          td.appendTo(tr);
+        }
+      });
+    }
+}
+
+function search_title(search) {
+      var url = $("#hid_site").val() + '/admin/problem/search_problem_byTitle';
+      $.post(url,{
+        title : search
+      },function(data) {
+        if(data == false) {
+          $("#tbody").html("");
+          $("#links").remove();
+          alert("抱歉没有找到");
+        } else {
+          var pro = eval("("+data+")");
+          $("#tbody").html("");
+          $("#links").remove();
+          var tr = $("<tr></tr>");
+          tr.appendTo(tbody);
+          td = $("<td>"+ pro.problem_id +"</td>");
+          td.appendTo(tr);
+          td = $("<td>"+ pro.title +"</td>");
+          td.appendTo(tr);
+          td = $("<td>"+ pro.class_name +"</td>");
+          td.appendTo(tr);
+          var url_ed = $("#hid_site").val() + '/admin/problem/pro_edit/' + pro.problem_id;
+          td = $("<td>"+ "<a href='" + url_ed + "'><i class='icon-pencil'></i></a>" +
+           "  <a href='#myModal' role='button' data-toggle='modal' id = '" + pro.problem_id + "'><i class='icon-remove'></i></a>" 
+           + "</td>");
+          td.appendTo(tr);
+        }
+      });
+}
