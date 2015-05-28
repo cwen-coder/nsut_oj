7ef81585be79f35276ec546c1bbdfe97e0333fcc
NBSUT <2484611837@qq.com>
2015-05-28 23:42:45 +0800

    题目状态Model controll测试

--- application/models/problemsubmit_model.php
+++ application/models/problemsubmit_model.php
@@ -13,21 +13,25 @@ class Problemsubmit_model extends CI_Model{
 		echo Now();*/
 		$sql_solution = "insert into solution(problem_id, user_id, in_date, language, ip, code_length, contest_id) values('$data[pid]', '$data[user_id]' , NOW(), '$data[language]', '$data[ip]', '$data[code_length]', '0')";
 		$sql_source = "insert into source_code(solution_id, source) values('$insert_id', '$data[source]')";
-		if($OJ_MEMCACHE){
+		/*if($OJ_MEMCACHE){
 				$this->load->library('CI_Memcache');
 				$mem = new CI_Memcache();
 				$mem->mc = $mem->init();
 				$mem->mc->delete(mad("mysql_query".$),0);
 			}
 		else{
-		
 		$result_solution = mysql_query($sql_solution);
 		$insert_id=mysql_insert_id();
-		
 		$result_source = mysql_query($sql_source); 
-		}
+		}*/
 	}
-	function problem_status(){
-		$sql = "select ";
+	function problem_status($limit, $num){
+		$sql = "select user_id, memory, time, result, language, code_length, solution_id, in_date, problem_id from solution order  by in_date DESC limit $limit, $num";
+		$result = mysql_query($sql);
+		$data = array();
+		while($row = mysql_fetch_assoc($result)){
+			$data[] = $row;
+		}
+		return $data;
 	}
 }
