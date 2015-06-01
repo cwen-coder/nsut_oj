c237767f84e8c0455f6e43f57203de0105f6da8e
yin-coder <172569856@qq.com>
2015-06-01 22:55:42 +0800

    调整

--- application/controllers/oj_index/home.php
+++ application/controllers/oj_index/home.php
@@ -67,6 +67,40 @@ class Home extends Oj_Controller{
 			$data['username'] = false;
 			$data['user_id'] = false;
 		}
+		$this->load->model('oj_con_model','oj_con');
+		$data['con_now'] = $this->oj_con->get_now_contest();
+		//分页获取已结束的比赛
+		$data['num'] =  $this->oj_con->pass_con_num();
+		//后台设置后缀为空，否则分页出错
+		$this->config->set_item('url_suffix', '');
+		//载入分页类
+		$this->load->library('pagination');
+		$perPage = 3;
+		//配置项设置
+		$config['base_url'] = site_url('oj_index/home/contest_list/');
+		$config['total_rows'] = $data['num']['count(*)'];
+		$config['per_page'] = $perPage;
+		$config['uri_segment'] = 4;
+		$config['first_link'] = '首页';
+		$config['prev_link'] = '上一页';
+		$config['next_link'] = '下一页';
+		$config['last_link'] = '尾页';
+		$config['full_tag_open'] = '';
+		$config['full_tag_close'] = '';
+		$config['cur_tag_open'] = '<li class="active"><a>'; // 当前页开始样式   
+		$config['cur_tag_close'] = '</a></li>'; 
+
+		$this->pagination->initialize($config);
+
+		$data['links'] = $this->pagination->create_links();	
+		$offset = $this->uri->segment(4);
+		if($offset < 1) $offset = 0;
+		//$this->db->limit($perPage, $offset);
+		$data['con_pass'] = $this->oj_con->con_pass_list($perPage, $offset);
+		/*p($data['con_now']);
+		p($data['con_pass']);
+		die;*/
+		//echo $data['links'];die;
 	 	$this->load->view('oj_index/contest_list.html',$data);
 	}
 	//提交状态显示
@@ -105,6 +139,7 @@ class Home extends Oj_Controller{
 
 	public function log_out() {
 		$this->session->sess_destroy();
-		success('oj_index/home/index','注销成功');
+		header('Content-Type:text/html;charset=utf-8');
+		echo "<script type='text/javascript'> alert('注销成功 ');history.go(-1); </script>";
 	}
 }
---
+++ application/models/oj_con_model.php
@@ -0,0 +1,40 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+* @author CWen Yin <[email address]>
+* @date(2015-6-1)
+*/
+class Oj_con_model extends CI_Model {
+	function __construct(){
+		parent::__construct();
+		require('db_info.inc.php');
+	}
+
+	//获取正在进行或是还未开始的比赛
+	public function get_now_contest() {
+		$query = "SELECT contest_id,title,con_class,start_time,end_time FROM contest WHERE end_time > NOW() and  (con_class = 1 or con_class = 2) ORDER BY contest_id DESC";
+		$result = mysql_query($query);
+		$data = array();
+		while ($row = mysql_fetch_array($result,MYSQL_ASSOC)) {
+			$data[] = $row;
+		}
+		return $data;
+	}
+
+	public function con_pass_list($perPage, $offset) {
+		$query = "SELECT contest_id,title,con_class,start_time,end_time FROM contest WHERE end_time < NOW() and (con_class = 1 or con_class = 2) ORDER BY contest_id DESC limit $offset,$perPage ";
+		$result = mysql_query($query);
+		$data = array();
+		while ($row = mysql_fetch_array($result,MYSQL_ASSOC)) {
+			$data[] = $row;
+		}
+		return $data;
+	}
+
+	//获取已结束比赛的总数
+	public function pass_con_num() {
+		$query = "SELECT count(*) FROM contest WHERE end_time < NOW() and (con_class = 1 or con_class = 2)";
+		$result = mysql_query($query);
+		return  mysql_fetch_assoc($result);
+	}
+}
+?>
