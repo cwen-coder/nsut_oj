e3e2467cfcf7237fe4bd2756371ebb51a95e7bb9
yin-coder <172569856@qq.com>
2015-07-17 20:55:10 +0800

    完成重判

--- application/models/reset_problem_model.php
+++ application/models/reset_problem_model.php
@@ -8,5 +8,40 @@ class Reset_problem_model extends CI_Model {
 		parent::__construct();
 		require('db_info.inc.php');
 	}
+
+	public function con_pro_reset($contest_id,$problem_id) {
+		/*$sql="UPDATE `solution` SET `result`=1 WHERE `problem_id`=".$rjpid;
+		mysql_query($sql) or die(mysql_error());
+		$sql="delete from `sim` WHERE `s_id` in (select solution_id from solution where `problem_id`=".$rjpid.")";
+		mysql_query($sql) or die(mysql_error());*/
+		$query = "UPDATE solution SET result = 1 WHERE contest_id = '$contest_id' AND problem_id = '$problem_id' ";
+		$result = mysql_query($query);
+		$query1 = "DELETE FROM sim WHERE s_id in (select solution_id from solution where contest_id = '$contest_id' AND problem_id = '$problem_id')";
+		$result1 = mysql_query($query1);
+		if($result && $result1) {
+			return true;
+		} else return false;
+	}
+
+	public function pro_reset($problem_id) {
+		$query = "UPDATE solution SET result = 1 WHERE contest_id = 0 AND problem_id = '$problem_id' ";
+		$result = mysql_query($query);
+		$query1 = "DELETE FROM sim WHERE s_id in (select solution_id from solution where contest_id = 0 AND problem_id = '$problem_id')";
+		$result1 = mysql_query($query1);
+		if($result && $result1) {
+			return true;
+		} else return false;
+
+	}
+
+	public function s_pro_reset($solution_id) {
+		$query = "UPDATE solution SET result = 1 WHERE solution_id = '$solution_id' ";
+		$result = mysql_query($query);
+		$query1 = "DELETE FROM sim WHERE s_id in (select solution_id from solution where solution_id = '$solution_id')";
+		$result1 = mysql_query($query1);
+		if($result && $result1) {
+			return true;
+		} else return false;
+	}
 }
 ?>
--- application/views/admin/js/reset_pro.js
+++ application/views/admin/js/reset_pro.js
@@ -46,4 +46,32 @@ $(document).ready(function() {
 		$(this).closest('li').prev().removeClass('active');
 		$(this).closest('li').prev().prev().removeClass('active');
 	});
+
+	$("#contest_id").bind('input propertychange blur',function() {
+		
+		var contest_id = $(this).val();
+		//console.log(contest_id);
+		var url = $("#hide_site").val() + '/admin/reset_problem/ajax_con_pro' ;
+		//console.log(url);
+		$.post(url,{
+			contest_id:contest_id
+		},function(data) {
+			data = eval("("+data+")");
+			//console.log(data);
+			$('#con_pro_id option').remove();
+			var len = data.pro.length;
+			//console.log(len);
+			//var sel = document.getElementByid('con_pro_id');
+			for(var i = 0; i < len; i++) {
+				//console.log(value);
+				if(data.pro[i] != null) {
+					//sel.options.add(new Option("name","id"));
+					var value = data.pro[i].problem_id;
+					//console.log(value);
+					var text = data.arr[data.pro[i].num];
+					$("#con_pro_id").append("<option value='"+value+"'>"+text+"</option>");
+				}
+			}
+		});
+	});
 });
