2e066789bf6d35719ac548ccf15b930d73d32a04
yin-coder <172569856@qq.com>
2015-10-12 22:32:44 +0800

    debug

--- application/controllers/contest/school_contest.php
+++ application/controllers/contest/school_contest.php
@@ -174,7 +174,7 @@ class School_contest extends Oj_Controller{
             $teamname = $this->input->post('teamname',TRUE);
             $phone = $this->input->post('phone',TRUE);
             $user_id = $this->session->userdata('user_id');
-            if(!isset($user_id) || $this->user_model->teamname_check($teamname, $contest_id, $user_id) > 0){
+            if(!$user_id || $this->user_model->teamname_check($teamname, $contest_id, $user_id) > 0){
                     echo false;
             }  else {
                 
